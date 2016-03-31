b77290045c234c2f708f5b4aae1dd37032d28899
s14004402 <s14004402@a-f002116-l.salsa.univ-amu.fr>
2016-03-31 09:12:21 +0200

    AccessToken fait

--- TwitterMiner/src/Etape0.java
+++ TwitterMiner/src/Etape0.java
@@ -3,6 +3,7 @@ import twitter4j.auth.AccessToken;
 
 public class Etape0 {
 
-	AccessToken access = new AccessToken()
+	AccessToken access = new AccessToken("2496888542-Lr7DdmQxvm6bVOdb0z2DYRHgenlJLarkPuhDPd6",
+			"9fK9gFDfIMLdmOyAkwDtvcNZBvRClVsM8SRlzQSwViwcF");
 	
 }
