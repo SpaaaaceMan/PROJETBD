a030a0f325d835429c010e50caf07011e5a08767
m14002147 <m14002147@a-f002115-l.salsa.univ-amu.fr>
2016-03-31 10:24:49 +0200

    Creation sous forme d'objet

--- TwitterMiner/src/Etape0.java
+++ TwitterMiner/src/Etape0.java
@@ -1,8 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Map;
 
-import twitter4j.Logger;
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
@@ -14,10 +15,62 @@ import twitter4j.conf.ConfigurationBuilder;
 
 
 public class Etape0 {
-	private final static Logger logger = Logger.getLogger(Etape0.class);
+	
+	private String OAuthConsumerKey;
+	private String OAuthConsumerSecret;
+	private String OAuthAccessToken;
+	private String OAuthAccessTokenSecret;
+	
+	private Twitter twitter;
+	
+	public Etape0 (String OAuthConsumerKey, String OAuthConsumerSecret
+			, String OAuthAccessToken, String OAuthAccessTokenSecret) {
+		
+		this.OAuthConsumerKey       = OAuthConsumerKey;
+		this.OAuthConsumerSecret    = OAuthConsumerSecret;
+		this.OAuthAccessToken       = OAuthAccessToken;
+		this.OAuthAccessTokenSecret = OAuthAccessTokenSecret;
+		
+		connect ();
+		
+	}
+	
+	public Etape0 () {
+		
+		this.OAuthConsumerKey       = "lTaMcd8utKwMCXbBTyNqsgZsz";
+		this.OAuthConsumerSecret    = "IEXIf7AQp0C0XiefYiTC14xurcFbSvlWqNcSHfaZQp8D4Pl4Re";
+		this.OAuthAccessToken       = "2496888542-Lr7DdmQxvm6bVOdb0z2DYRHgenlJLarkPuhDPd6";
+		this.OAuthAccessTokenSecret = "9fK9gFDfIMLdmOyAkwDtvcNZBvRClVsM8SRlzQSwViwcF";
+		
+		connect ();
+	}
+	
+	private Boolean connect () {
+		System.out.println("Connection...");
+		
+		ConfigurationBuilder builder = new ConfigurationBuilder();
+		builder.setOAuthConsumerKey(OAuthConsumerKey);
+		builder.setOAuthConsumerSecret(OAuthConsumerSecret);
+		
+		builder.setOAuthAccessToken(OAuthAccessToken);
+		builder.setOAuthAccessTokenSecret(OAuthAccessTokenSecret);
+		Configuration configuration = builder.build();
+		
+		TwitterFactory factory = new TwitterFactory(configuration);
+		Twitter twitter = factory.getInstance();
+		
+		System.out.println("Connected...");
+		this.twitter = twitter;
+		return true;
+	}
+	
+	public List<Map<String, String>> searchBy () {
+		
+		return null;		
+	}
 
 	public static void main(String[] args) {
-		logger.info("Retrieving tweets...");
+		System.out.println("Connection...");
 		
 		ConfigurationBuilder builder = new ConfigurationBuilder();
 		builder.setOAuthConsumerKey("lTaMcd8utKwMCXbBTyNqsgZsz");
@@ -30,8 +83,7 @@ public class Etape0 {
 		TwitterFactory factory = new TwitterFactory(configuration);
 		Twitter twitter = factory.getInstance();
 		
-		System.out.println("key:" + twitter.getConfiguration().getOAuthConsumerKey());
-		System.out.println("secret: " + twitter.getConfiguration().getOAuthConsumerSecret());
+		System.out.println("Connected...");
         
 		String recherche = "";
 
@@ -67,7 +119,10 @@ public class Etape0 {
 
 		//System.out.println("Count : " + resultQuery.getTweets().size()) ;
 		
-        logger.info("done! ");
+		System.out.println("Done !");
 	}
-	
+
+	public Twitter getTwitter() {
+		return twitter;
+	}	
 }
