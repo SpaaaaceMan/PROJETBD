e19677db0f1e639be5d488924168c7b556c86ef0
t14007203 <t14007203@a-f002117-l.salsa.univ-amu.fr>
2016-03-31 09:42:44 +0200

    ajout nouvelles fonctions de csvfile

--- TwitterMiner/src/CSVFile.java
+++ TwitterMiner/src/CSVFile.java
@@ -1,6 +1,90 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 
 public class CSVFile {
 
+	private File file;
+    private char separator;
+    
+    public CSVFile(File file, char separator) {
+        if (file == null) 
+            throw new IllegalArgumentException("Le fichier ne peut pas etre nul");
+        this.file = file;
+        this.separator = separator;
+    }
+    
+    public void write(List<Map<String, String>> mappedData, String[] titles) throws IOException {
+
+        if (mappedData == null) 
+            throw new IllegalArgumentException("la liste ne peut pas être nulle");
+        if (titles == null)
+            throw new IllegalArgumentException("les titres ne peuvent pas être nuls");
+        FileWriter fw = null;
+		try {
+			fw = new FileWriter(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        // Les titres
+        boolean first = true;
+        for (String title : titles) {
+            if (first)
+                first = false;
+            else
+                bw.write(separator);
+            write(title, bw);
+        }
+        bw.write("\n");
+
+        // Les données
+        for (Map<String, String> oneData : mappedData) {
+            first = true;
+            for (String title : titles) {
+                if (first)
+                    first = false;
+                else
+                    bw.write(separator);
+                final String value = oneData.get(title);
+                write(value, bw);
+
+            }
+            bw.write("\n");
+            bw.close();
+            fw.close();
+        }
+    }
+    
+    private void write(String value, BufferedWriter bw) throws IOException {
+        if (value == null)
+            value = "";
+        
+        boolean needQuote = false;
+
+        if (value.indexOf("\n") != -1)
+            needQuote = true;
+
+        if (value.indexOf(separator) != -1)
+            needQuote = true;
+
+        if (value.indexOf("\"") != -1) {
+            needQuote = true;
+            value = value.replaceAll("\"", "\"\"");
+        }
+
+        if(needQuote)
+            value = "\"" + value + "\"";
+
+        bw.write(value);
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
