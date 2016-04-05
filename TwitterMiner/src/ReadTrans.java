0e6af2c9b4b8e2f30debff98a2a33937d8fe4e7f
dark1wador <m14002147@a-f002088-l.salsa.univ-amu.fr>
2016-04-05 11:52:25 +0200

    Modification de ReadTrans pour intégré des arguments

--- TwitterMiner/src/ReadTrans.java
+++ TwitterMiner/src/ReadTrans.java
@@ -10,48 +10,56 @@ import java.util.Map;
 public class ReadTrans {
 
 	public static void main(String[] args) {
-		try (BufferedReader br = new BufferedReader(new FileReader("src/test.trans")))
-		{
-			String FILE_NAME = "src/" + "result.csv";
-			File file = CSVFile.getResource(FILE_NAME);
-			CSVFile CSV = new CSVFile(file, ';');
-			
-			ArrayList<String> wantedTitles = new ArrayList<String>();
-			String currentLine;
-			List<Map<String, String>> data = new ArrayList<Map<String, String>>();
-			int max = 0;
-			
-			while ((currentLine = br.readLine()) != null) {
+		if (args.length < 2) {
+			System.out.println("error : not enough arguments. Usage : ReadTrans <Trans file file path (must exist)>" +
+					"<Csv path(parent repositeray must exists)>");
+		}
+		else {
+			try (BufferedReader br = new BufferedReader(new FileReader(args[0])))
+			{
+				String FILE_NAME = args[1];
+				File file = CSVFile.getResource(FILE_NAME);
+				file.createNewFile();
+				CSVFile csv = new CSVFile(file, ';');
 				
-				String[] currentSplitLine = currentLine.split(" ");
+				ArrayList<String> wantedTitles = new ArrayList<String>();
+				String currentLine;
+				List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+				int max = 0;
 				
-				Map<String, String> oneData = new HashMap<String, String>();
-				
-				for (int i = 0; i < currentSplitLine.length; ++i){
-					System.out.println("taille " + currentSplitLine.length);
-					if (i != currentSplitLine.length - 1){
-						oneData.put("Motif " + i, currentSplitLine[i]);
+				while ((currentLine = br.readLine()) != null) {
+					
+					String[] currentSplitLine = currentLine.split(" ");
+					
+					Map<String, String> oneData = new HashMap<String, String>();
+					
+					for (int i = 0; i < currentSplitLine.length; ++i){
+						System.out.println("taille " + currentSplitLine.length);
+						if (i != currentSplitLine.length - 1){
+							oneData.put("Motif " + i, currentSplitLine[i]);
+						}
+						else{
+							oneData.put("Support", currentSplitLine[i]);
+						}
+						if (i > max)
+							max = i;
 					}
-					else{
-						oneData.put("Support", currentSplitLine[i]);
-					}
-					if (i > max)
-						max = i;
+					System.out.println("endline");
+			        data.add(oneData);
 				}
-				System.out.println("endline");
-		        data.add(oneData);
-			}
-			System.out.println("max = " + max);
-			for (int i = 0; i < max + 1; ++i){
-				if (i != max)
-					wantedTitles.add("Motif " + i);
-				else
-				wantedTitles.add("Support");
-			}
-			CSV.writeTrans(data, wantedTitles);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} 
+				System.out.println("max = " + max);
+				for (int i = 0; i < max + 1; ++i){
+					if (i != max)
+						wantedTitles.add("Motif " + i);
+					else
+					wantedTitles.add("Support");
+				}
+				csv.writeTrans(data, wantedTitles);
+	
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
+			} 
+		}
 	}
 }
