e9aac3785aea28552718cb71d05b1164b5af3bac
dark1wador <m14002147@a-f002088-l.salsa.univ-amu.fr>
2016-04-05 10:35:30 +0200

    Ajout Etape 1: conversion Csv en Trans

---
+++ TwitterMiner/src/Etape1.java
@@ -0,0 +1,143 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class Etape1 {
+	
+	/**
+     * Function to convert the CSV File to a Trans File for the apriori algorithim
+     */
+    public static File convertToTrans(String transFilePath, String csvFilePath, char separator) {
+    	//Create the file
+    	File transFile = null;
+    
+    	//Initialize the Buffered Stuff
+    	BufferedReader in  = null;
+    	BufferedWriter out = null;
+    	
+    	//Each CSV line
+    	String line = "";
+
+    	try {
+
+    		//Create the transFile
+    		transFile = CSVFile.getResource(transFilePath);
+    		//Create the new transfile if it doesn't exist
+    		transFile.createNewFile();
+    		
+    		//Read each line from the CSV
+			in = new BufferedReader(new FileReader(CSVFile.getResource(csvFilePath)));
+			//This is for ignoring the line with titles
+			line = in.readLine();
+			
+			//This contains already found words
+			List<String> referenceArray = new ArrayList<String>();
+			
+			//This contains the number corresponding to each word
+			Map<String, Integer> referenceMap = new HashMap<String, Integer>();
+			//The max int
+			Integer maxInt = 0;
+			
+			//Results
+			List<List<String>> result = new ArrayList<List<String>>();
+			
+			while ((line = in.readLine()) != null) {
+				
+			    // use the given separator to split the CSV
+				String[] status = line.split(Character.toString(separator));
+				
+				//The current splited line
+				List<String> currentLine = new ArrayList<String>();
+				
+				//Put the String[] into an ArrayList<String>
+				Collections.addAll(currentLine, status);
+				
+				//Contains the current line converted
+				List<String> resultLine = new ArrayList<String>();
+				
+				//Look in the current line
+				for (int i = 2; i < currentLine.size(); ++i) {
+					//Since we need an item Set, we ignore already processed words ine a status
+					if (status[i] == "" || resultLine.contains(referenceMap.get(currentLine.get(i)) + " ")) {
+						continue;
+					}
+					//If the words hasn't been found yet, add it to the reference Array
+					if (!referenceArray.contains(currentLine.get(i))) {
+						referenceArray.add(currentLine.get(i));
+								
+						//Increment the maxInt
+						++maxInt;
+						
+						referenceMap.put(currentLine.get(i), maxInt);
+					}
+					//Then we get the corresponding Integer
+					resultLine.add(referenceMap.get(currentLine.get(i)).toString() + " ");
+				}
+				
+				//And we add the result line to the final result
+				result.add(resultLine);
+				
+				//Display the input
+				for (int i = 2; i < status.length; ++i) {
+					System.out.print(" " + status[i]);
+				}
+				
+				System.out.println("line finished");
+			}
+			
+			System.out.println("Finished reading CSV file");
+			
+			//Creating the BufferedWriter on the trans file
+			out = new BufferedWriter(new FileWriter(transFile));
+			
+			//Begin writing
+			for (List<String> displayedLine : result) {
+				for (String displayStr : displayedLine) {
+					System.out.print(displayStr);
+					out.write(displayStr);
+				}
+				System.out.println();
+				out.newLine();;
+			}
+			
+    	} 
+    	catch (FileNotFoundException e) {
+    		e.printStackTrace();
+    	} 
+    	catch (IOException e) {
+    		e.printStackTrace();
+    	} 
+    	//Close the buffered Stuff
+    	finally {
+    		try {
+    			
+	    		if (in != null) {
+	    			in.close();		
+	    		}
+	    		if (out != null) {
+	    			out.close();
+	    		}
+    		}
+    		catch (IOException e) {
+				e.printStackTrace();
+			}
+    	}
+    	System.out.println("Done");
+		return transFile;
+    }
+
+	public static void main(String[] args) {
+		Etape1.convertToTrans("src/test.trans", "src/testConv.csv", ',');
+	}
+
+}
